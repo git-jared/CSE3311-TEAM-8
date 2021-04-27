b7794457d0787dcd7f8c8dd33838b1d43e3e9006
ayush7864 <60585838+ayush7864@users.noreply.github.com>
2021-04-26 20:07:26 -0500

    Update Header.js

--- Header.js
+++ Header.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     headerTitle: {
-        color: 'black',
+        color: 'white',
         fontSize: 15*PixelRatio.get(),
         fontWeight: 'bold'
     }
